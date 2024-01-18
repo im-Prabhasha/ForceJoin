33129b57015129923c5992ea9c9aadc70d569851
im-Prabhasha <153989328+im-Prabhasha@users.noreply.github.com>
2024-01-18 11:27:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,33 +3,28 @@ from pyrogram import Client
 # Replace 'YOUR_API_ID_1', 'YOUR_API_HASH_1', 'YOUR_BOT_TOKEN_1',
 # 'YOUR_API_ID_2', 'YOUR_API_HASH_2', 'YOUR_BOT_TOKEN_2', and so on
 # with the actual values.
-api_ids = ['YOUR_API_ID_1', 'YOUR_API_ID_2', 'YOUR_API_ID_3', 'YOUR_API_ID_4', 'YOUR_API_ID_5',
-           'YOUR_API_ID_6', 'YOUR_API_ID_7', 'YOUR_API_ID_8', 'YOUR_API_ID_9', 'YOUR_API_ID_10']
+api_ids = ['29301453', '24365770']
 
-api_hashes = ['YOUR_API_HASH_1', 'YOUR_API_HASH_2', 'YOUR_API_HASH_3', 'YOUR_API_HASH_4', 'YOUR_API_HASH_5',
-              'YOUR_API_HASH_6', 'YOUR_API_HASH_7', 'YOUR_API_HASH_8', 'YOUR_API_HASH_9', 'YOUR_API_HASH_10']
+api_hashes = ['aaf265964f64b744218fbebdd79c5319', '29afc2eeacd1741455d285cf48bb1c22']
 
-bot_tokens = ['YOUR_BOT_TOKEN_1', 'YOUR_BOT_TOKEN_2', 'YOUR_BOT_TOKEN_3', 'YOUR_BOT_TOKEN_4', 'YOUR_BOT_TOKEN_5',
-              'YOUR_BOT_TOKEN_6', 'YOUR_BOT_TOKEN_7', 'YOUR_BOT_TOKEN_8', 'YOUR_BOT_TOKEN_9', 'YOUR_BOT_TOKEN_10']
 
 # Replace 'your_channel_username' with your actual channel username
-channel_username = '@your_channel_username'
+channel_username = '@sinhalastatus'
 
-def join_channel(api_id, api_hash, bot_token):
+def join_channel(api_ids, api_hashs):
     app = Client(
         "bot",
-        api_id=api_id,
-        api_hash=api_hash,
-        bot_token=bot_token
+        api_id=api_ids,
+        api_hash=api_hashs
     )
 
     with app:
         app.join_chat(channel_username)
-        print(f"Bot with token {bot_token} has joined the channel.")
+        print(f"Bot with token im has joined the channel.")
 
 def main():
-    for i in range(10):
-        join_channel(api_ids[i], api_hashes[i], bot_tokens[i])
+    for i in range(2):
+        join_channel(api_ids[i], api_hashes[i])
 
 if __name__ == "__main__":
     main()
